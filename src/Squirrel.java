7ce70f2e3be0d5ff56127da1a3119205d1480198
Semenov_Atryom <arsem.personal@gmail.com>
2022-02-17 17:58:25 +0300

    Second commit

--- src/Squirrel.java
+++ src/Squirrel.java
@@ -15,25 +15,18 @@ public class Squirrel {
         if (len > 1) {
             int i = 0;
             //Проверяем, можно ли подобрать орешки с первого
-            if (nuts[i] > 0)
-                maxOfNuts += nuts[i];
+            if (nuts[i] > 0) maxOfNuts += nuts[i];
             //Проверяем, можно ли подобрать орешки со второго до последнего-предпоследнего
             for (; i < len - 2; i++) {
-                if (nuts[i + 1] < 0 && nuts[i + 1] <= nuts[i + 2])
-                    i++;//перепрыгиваем через пень
+                if (nuts[i + 1] < 0 && nuts[i + 1] <= nuts[i + 2]) i++;//перепрыгиваем через пень
                 maxOfNuts += nuts[i + 1];
-                if (maxOfNuts < 0)
-                    maxOfNuts = 0;
+                if (maxOfNuts < 0) maxOfNuts = 0;
             }
             // Смотрим, стоит ли прыгать на последний пень
-            if (i == len - 2 && nuts[i + 1] > 0) {
-                maxOfNuts += nuts[i + 1];
-            }
+            if (i == len - 2 && nuts[i + 1] > 0) maxOfNuts += nuts[i + 1];
             return maxOfNuts;
-        } else if (nuts[0] >= 0)
-            return nuts[0];
-        else
-            return 0;
+        } else if (nuts[0] >= 0) return nuts[0];
+        else return 0;
     }
     //*/
 }
